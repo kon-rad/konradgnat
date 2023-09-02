066e5aa2a05d2b1f05d27a03ff57bb5bfbbe6969
Konrad Gnat <konradmgnat@gmail.com>
2023-09-01 23:06:09 -0500

    update debuging bild error

--- app/nextjs/next.config.js
+++ app/nextjs/next.config.js
@@ -10,6 +10,5 @@ module.exports = {
   },
   productionBrowserSourceMaps: false, // Disable source maps in development
   optimizeFonts: false, // Disable font optimization
-  fastRefresh: true,
   swcMinify: true,
 };
